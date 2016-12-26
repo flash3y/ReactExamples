9348546a8edd9e19151cbc94380fa10f3bd65f68
Peter Korosec <flash3y@gmail.com>
2016-12-26 20:54:00 +0100

    Prepare for heroku

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+
 app.use(express.static('./build'));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
-app.listen(9000);
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
+});
