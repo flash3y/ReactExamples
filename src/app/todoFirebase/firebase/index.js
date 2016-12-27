560fb62855fa071299eb11e9dc14791f13032114
Peter Korosec <flash3y@gmail.com>
2016-12-27 17:11:35 +0100

    react-router update and .env addition

--- src/app/todoFirebase/firebase/index.js
+++ src/app/todoFirebase/firebase/index.js
@@ -1,20 +1,24 @@
 import firebase from 'firebase';
 
 try {
-  //var config = {
-  //  apiKey: process.env.API_KEY,
-  //  authDomain: process.env.AUTH_DOMAIN,
-  //  databaseURL: process.env.DATABASE_URL,
-  //  storageBucket: process.env.STORAGE_BUCKET,
-  //};
-
-  var config = {
-    apiKey: "AIzaSyAyJ_sxc40zhFHV132UdBZQMreGsm7AyBg",
-    authDomain: "todoapp-42a4a.firebaseapp.com",
-    databaseURL: "https://todoapp-42a4a.firebaseio.com",
-    storageBucket: "todoapp-42a4a.appspot.com",
-    messagingSenderId: "1041824414628"
-  };
+  if (process.env.NODE_ENV === 'test'){
+    var config = {
+      apiKey: process.env.REACT_APP_TEST_API_KEY,
+      authDomain: process.env.REACT_APP_TEST_AUTH_DOMAIN,
+      databaseURL: process.env.REACT_APP_TEST_DATABASE_URL,
+      storageBucket: process.env.REACT_APP_TEST_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_TEST_SENDER_ID
+    };
+  }
+  else {
+    var config = {
+      apiKey: process.env.REACT_APP_DEV_API_KEY,
+      authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
+      databaseURL: process.env.REACT_APP_DEV_DATABASE_URL,
+      storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_DEV_SENDER_ID
+    }; 
+  }
   firebase.initializeApp(config);
 } catch (e) {
 
